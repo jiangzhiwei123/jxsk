2d427b29eba3b65b0a687858772ced286c4f4949
jiangzhiwei123 <320826642@qq.com>
2019-06-03 21:56:11 +0800

    6 3

--- src/api/loadSimple.js
+++ src/api/loadSimple.js
@@ -4,7 +4,7 @@ import mpx from '@mpxjs/core'
 export default class SimpleActive {
   static async getAll() {
     const res = await Http.get({ url: '/activity/loadSimpleActivityInfo' })
-    console.log(444, res)
+    console.log(444555666, res)
     return res
   }
 }
--- src/store/simpList.js
+++ src/store/simpList.js
@@ -7,18 +7,32 @@ import SimpleActive from '../api/loadSimple'
  * 这里是拆分store的例子
  */
 const state = {
-  simpleList: []
+  // 显示的列表
+  simpleList: [],
+  // 显示的页数
+  pageCount:1
+
 }
 const mutations = {
   // 更新缩略活动
   updateSimple(state, p) {
     state.simpleList = p
+  },
+  // 更新页数
+  updatePage(state, p) {
+    state.pageCount += p
   }
 }
 const actions = {
   // 缩略发布的活动部分
-  async fetchSimple(context, type) {
-    const simple = await SimpleActive.getAll()
+  async fetchSimple(context,{type,rows,page}) {
+    const simple = await Http.get({
+      url:'/activity/loadSimpleActivityInfo',
+      data:{
+        rows,
+        page
+      }
+    })
     if (type === '') {
       context.commit('updateSimple', simple.data)
     } else if (type === '企业') {
@@ -27,12 +41,14 @@ const actions = {
       context.commit('updateSimple', simple.data.filter(item => item.plateName === '校园'))
     }
   },
-  async activePro({ commit }, { name, id }) {
+  async activePro({ commit }, { name, id ,rows,page}) {
     const res = await Http.get({
       url: '/activity/loadSimpleActivityInfo',
       data: {
         proviceId: id,
-        plateName: name
+        plateName: name,
+        rows,
+        page
       }
     })
     console.log(88888888888, res.data)
